de3e5c61a591ed5a1cc05d88180810d651e1495e
AbhishekSingh198 <pabhishek198@gmail.com>
2021-06-28 12:16:02 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const tabs= document.querySelectorAll(".tab");
+tabs.forEach((clickedTab)=>{
+    clickedTab.addEventListener('click',()=>{
+        tabs.forEach((tab=>{
+            tab.classList.remove("active");
+        }))
+        clickedTab.classList.add("active");
+        const clickedTabBGColor=getComputedStyle
+        (clickedTab).getPropertyValue(
+            "color"
+        );
+        document.body.style.background=clickedTabBGColor;
+    });
+});
